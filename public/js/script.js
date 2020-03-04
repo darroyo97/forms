dc9fc74952391452b2625411270efc537932f406
Daniela <daniela.arroyo27@gmail.com>
2020-03-04 11:25:31 -0600

    form submit

--- public/js/script.js
+++ public/js/script.js
@@ -3,3 +3,39 @@
 // myButton.addEventListener('click', (e) => {
 //     alert('we clicked a button')
 // })
+
+// fetch('url', {
+//     method: "POST",
+//     body: "json data"
+// })
+//     .then()
+let form = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(e)
+    let email = document.getElementById('email')
+    let fname = document.getElementById('fname')
+    let number = document.getElementById('number')
+    let lname = document.getElementById('lname')
+    let zip = document.getElementById('zip')
+
+    fetch('/', {
+        method: "POST",
+        header: { 'Content-type': 'application/json' },
+        body: {
+            'email': email.value,
+            'lname': lname.value,
+            'fname': fname.value,
+            'zip': zip.value,
+            'number': number.value
+        }
+    })
+        .then((response) => {
+            let msg = document.getElementById('msg');
+            msg.textContent = "Your form was submitted"
+        })
+        .catch(() => {
+            let msg = document.getElementById('msg');
+            msg.textContent = "error"
+        })
+})
--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const body_parser = require('body-parser');
-
 router.use(body_parser.urlencoded({ extended: false }));
-
 
 router.get('/', (req, res) => {
     res.render('index.ejs')
 })
-// router.post('/', (req, res) => {
-//     console.log(req.body)
-//     // let email = req.body.email
-//     // let isTrue = req.body.isTrue
-//     // let isValid = req.body.isValid
-//     // let password = req.body.password
-//     // res.send('posted data')
-//     // res.send(`email: ${email} isTrue: ${isTrue} isValid ${isValid} password: ${password}`)
-//     res.render('index', {
-//         // email,
-//         // isTrue,
-//         // isValid,
-//         // password
-//         // email: email,
-//         // isTrue: isTrue,
-//         // isValid: isValid,
-//         // password: password
-//     })
-// })
+
 router.post('/', (req, res) => {
     let firstName = req.body.firstName
     let lastName = req.body.lastName
